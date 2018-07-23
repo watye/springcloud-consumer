3f881a857fee84e7180cebcfd655eb1dd2a26bd0
watye <watye@qq.com>
2018-07-23 16:21:26 +0800

    添加配置中心

--- src/main/java/com/talelife/myproject/controller/UserController.java
+++ src/main/java/com/talelife/myproject/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.talelife.myproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.talelife.myproject.feign.UserWebService;
+import com.talelife.myproject.feign.dto.UserDto;
 import com.talelife.util.Result;
 
 @RestController
@@ -14,8 +16,8 @@ public class UserController extends BaseController{
 	@Autowired
 	private UserWebService userWebService;
 	
-	@RequestMapping("/get_user")
-	public Result getUser(Long id){
+	@GetMapping("/get_user")
+	public Result<UserDto> getUser(Long id){
 		return userWebService.getUser(id);
 	}
 	
